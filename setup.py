5dae6e18d4e810577446db31c0f07cf61341f4f8
Jiaxin Li <jil4025@med.cornell.edu>
2024-01-26 21:04:40 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='small_tools',
+    version='0.1',
+    author='Jiaxin Li',
+    author_email='jil4025@med.cornell.edu',
+    description='Some practical tools',
+    packages=find_packages(),
+    install_requires=['numpy',
+                      'requests',
+                     ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8'
+)
